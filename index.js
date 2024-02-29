931f7955743324c1fbf849614509cbde35ce14d9
nholdorf <160804768+nholdorf@users.noreply.github.com>
2024-02-28 19:56:16 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+  }
+  writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+  function countDown (number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i)
+        i--;
+    }
+  }
+ 
